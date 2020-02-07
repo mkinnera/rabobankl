22af700100768f62a915f3430ec9d76fb82df090
kinnera_d <Kinnu12345*>
2020-02-07 14:57:38 +0530

    swagger and controller changes with request and response.

---
+++ src/test/java/com/cts/rabobank/controller/CustomerStatementControllerTest.java
@@ -0,0 +1,8 @@
+package com.cts.rabobank.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerStatementControllerTest {
+}
