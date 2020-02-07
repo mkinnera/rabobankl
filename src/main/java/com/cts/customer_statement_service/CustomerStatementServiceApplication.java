5d8e8d65898499ddd6ea26a388b9364653934fb1
venkateshkumard <venkateshkumar_d@yahoo.com>
2020-02-07 12:24:17 +0530

    src
    src checked in

---
+++ src/main/java/com/cts/customer_statement_service/CustomerStatementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cts.customer_statement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerStatementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerStatementServiceApplication.class, args);
+    }
+
+}
