18ecb4dde6052177fac31dc271b7b3fd075dfcf5
kinnera_d <Kinnu12345*>
2020-02-07 14:34:48 +0530

    swagger and controller changes with request and response.

--- src/main/java/com/cts/rabobank/controller/CustomerStatementController.java
+++ src/main/java/com/cts/rabobank/controller/CustomerStatementController.java
@@ -3,16 +3,15 @@ package com.cts.rabobank.controller;
 import com.cts.rabobank.exception.RecordException;
 import com.cts.rabobank.model.ResponseRecord;
 import com.cts.rabobank.service.CustomerStatementProcessorService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/v1")
+@Slf4j
 public class CustomerStatementController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerStatementController.class);
     @Autowired
     CustomerStatementProcessorService customerStatementProcessorService;
 
@@ -21,17 +20,16 @@ public class CustomerStatementController {
 
         if (multipartFile != null) {
             String contentType = multipartFile.getContentType();
-            LOGGER.info("contentType{}", contentType);
+            log.info("contentType{}", contentType);
             if (contentType != null && (contentType.equals("text/xml") || contentType.equals("text/csv"))) {
                return customerStatementProcessorService.process(multipartFile, contentType);
             } else {
-                throw new RecordException("invalid formate exception");
+                throw new RecordException("Invalid file formate exception");
             }
         }
         return null;
     }
-
-    @GetMapping(value = "/dummy")
+    @GetMapping(value = "/test")
     public String customerStatementProcessor() {
         return "testing";
     }
