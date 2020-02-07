9045bb3b930a1d7218542bf1a0ca01ce3d2d5fb3
kinnera_d <Kinnu12345*>
2020-02-07 13:56:16 +0530

    service layer commit

--- src/main/java/com/cts/customer_statement/controller/CustomerStatementController.java
+++ src/main/java/com/cts/customer_statement/controller/CustomerStatementController.java
@@ -1,23 +1,22 @@
 package com.cts.customer_statement.controller;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/rabobank")
-
+@RequestMapping("/v1")
 public class CustomerStatementController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerStatementController.class);
 
-    @GetMapping("/upload")
-    public String customerStatementProcessor(@RequestParam MultipartFile multipartFile){
+    @GetMapping(value = "/rabobank")
+    public String customerStatementProcessor(@RequestParam ("file") MultipartFile multipartFile){
 
         if(multipartFile!=null){
             String contentType= multipartFile.getContentType();
             LOGGER.info("contentType{}",contentType);
+           // if(contentType!=null && contentType.equals(""))
         }
 
       return "testing";
