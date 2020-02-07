ff87fa3ec4e1f358e845b8256a82f4075699c24d
venkat <venkateshkumar_d@yahoo.com>
2020-02-07 12:41:21 +0530

    Controller Skeleton

---
+++ src/main/java/com/cts/customer_statement_service/controller/CustomerStatementController.java
@@ -0,0 +1,25 @@
+package com.cts.customer_statement_service.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/rabobank")
+
+public class CustomerStatementController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerStatementController.class);
+
+    @GetMapping("/upload")
+    public String customerStatementProcessor(@RequestParam MultipartFile multipartFile){
+
+        if(multipartFile!=null){
+            String contentType= multipartFile.getContentType();
+            LOGGER.info("contentType{}",contentType);
+        }
+
+      return "testing";
+    }
+}
